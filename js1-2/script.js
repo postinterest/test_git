14d08276476428a5770d8306d828851c884b67a1
Denis Lodov <ilodov@gmail.com>
2016-03-18 22:46:46 +0300

    dz_js1-2

--- js1-2/script.js
+++ js1-2/script.js
@@ -1,103 +1,58 @@
-/*var arr = ['alex', 'saho', 'denis', 'dima', 'miho'];
+//объявляем переменные в которые будем записывать числа, которые ввёл пользователь
+var firstNumber, secondNumber;
 
-console.log ('arr =', arr[2]);
+//объявляем функцию для ввода данных
+function getUserInput(){
+  firstNumber = prompt('введите число, которое необходимо возвести в степень', '');
+	secondNumber = prompt('введите степень, в которую необходимо возвести число', '');
+}
 
-for (var i = arr.length-1; i >= 0; i--) {
+//объявляем функцию для возведения в степень
+function pow (a, b) {
+  var result = a;
+  for ( i = 1; i < b; i++) {
+       result *= a;    //это сокращенная форма записи. если писать полностью то тут написано result = result * a;
+    }
+    return result;
+ }
+
+//функция для начала всей нашей программы. её можно зациклить 
+function start(){
+  
+  getUserInput(); //вызов функции для ввода данных
+  
+  if (secondNumber <= 1){
+    alert('Степень ' + secondNumber + 'не поддерживается, введите целую степень, большую 1');
+    start();  //перезапуск если была введена неправильная степень
+  }
+  else {  
+    alert( pow(firstNumber, secondNumber) );
+  }
+}
+
+//вызов
+
+
+//начало программы по анализу имени
+function nameStart(){
+var arr = [];
+
+for (var i = 0; i < 5; i++) {
 	
-	console.log ('i = ', i);
-	console.log ('arr[i] = ', arr[i]);	
+arr.push(prompt('Задайте имя'));
+
+}
+console.log(arr);
+var yourName = prompt('Введите ваше имя');
+var mistake = 'Ошибка входа';
+for (var i = 0; i < arr.length; i++) {
+
+	if (yourName == arr[i]) {
+		
+	mistake = (yourName + ', это заявка на успех!')	
+	}
+ 
 }
 
-alert ('хорош уже');
-console.log (arr.length);
-
-arr.push ('semen');
-console.log ('massiv ', arr);
-
-if (arr.length > 4 )  {
-	
-	console.log ('arr[i last] = ', arr[5]);
-	var itemplus = arr.push();
-	console.log ('itemplus ', itemplus); 	
+alert(mistake);
 }
-
-arr.unshift ('нежданчик');
-console.log (arr);
-
-arr.pop ();
-var lastdelete = arr.pop();
-
-console.log (lastdelete);
-
-var objekt = {
-	first: 'floor',
-	second: 'room',
-	third: 'kitchen',
-	'its your love': 7
-}
-
-for (var key in objekt) {
-	console.log (key);
-	console.log (objekt[key])
-}
-
-var keylist = Object.keys(objekt);
-console.log (keylist);*/
-
-/*var str = '12px';
-var num = parseInt (str);
-var newnum = num + '';
-console.log (typeof newnum);
-
-var newnomber = 12.3;
-var roundResult = Math.round(newnomber);
-console.log (roundResult);*/
-
-/*var arr = [1,3,5,2];
-var newArr = arr.sort(); //сортировка в массиве
-
-console.log (newArr);
-
-arr.push ('test sstring'); //добавление в конец массива один элемент
-
-console.log (newArr);
-
-arr.pop (''); // удаление из массива последний элемент
-
-console.log (newArr);*/
-
-
-
-function pow(a) {
-	var firstNum = prompt('введите число, которое необходимо возвести в степень', '');
-	console.log('число ', firstNum);
-	var secondNum = prompt('введите степень, в которую необходимо возвести число', '');
-	console.log('в степени ', secondNum);
-
-
-
-
-			if (firstNum <= 99) {
-				if (secondNum <= 99) {
-					
-					for (i = 0; i < secondNum; i++) {
-						var result = 1;
-						result *= firstNum
-						var finalResult = result * firstNum;
-					}
-					
-				alert (finalResult);
-
-				console.log ('результат = ', finalResult);}
-				else{
-			    alert ('введите число до 99')};
-			} 
-			else {
-			alert ('введите число до 99')}; 
-
-
-	
-}
-
-
-//var calculateResult = calculate(2, 3, 4);
---
+++ script_lessons2.js
@@ -0,0 +1,67 @@
+/*var arr = ['alex', 'saho', 'denis', 'dima', 'miho'];
+
+console.log ('arr =', arr[2]);
+
+for (var i = arr.length-1; i >= 0; i--) {
+	
+	console.log ('i = ', i);
+	console.log ('arr[i] = ', arr[i]);	
+}
+
+alert ('хорош уже');
+console.log (arr.length);
+
+arr.push ('semen');
+console.log ('massiv ', arr);
+
+if (arr.length > 4 )  {
+	
+	console.log ('arr[i last] = ', arr[5]);
+	var itemplus = arr.push();
+	console.log ('itemplus ', itemplus); 	
+}
+
+arr.unshift ('нежданчик');
+console.log (arr);
+
+arr.pop ();
+var lastdelete = arr.pop();
+
+console.log (lastdelete);
+
+var objekt = {
+	first: 'floor',
+	second: 'room',
+	third: 'kitchen',
+	'its your love': 7
+}
+
+for (var key in objekt) {
+	console.log (key);
+	console.log (objekt[key])
+}
+
+var keylist = Object.keys(objekt);
+console.log (keylist);*/
+
+/*var str = '12px';
+var num = parseInt (str);
+var newnum = num + '';
+console.log (typeof newnum);
+
+var newnomber = 12.3;
+var roundResult = Math.round(newnomber);
+console.log (roundResult);*/
+
+/*var arr = [1,3,5,2];
+var newArr = arr.sort(); //сортировка в массиве
+
+console.log (newArr);
+
+arr.push ('test sstring'); //добавление в конец массива один элемент
+
+console.log (newArr);
+
+arr.pop (''); // удаление из массива последний элемент
+
+console.log (newArr);*/
